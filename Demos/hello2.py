7d8473d9335723f24e0f07c0b37e1f8b1c2870f6
Michiel de Hoon <mjldehoon@yahoo.com>
2016-09-10 13:09:59 +0900

    hello2

--- Demos/hello2.py
+++ Demos/hello2.py
@@ -1,11 +1,22 @@
-from guitk import gui
+from guitk import gui, layout
 
 def say_hi():
     print "hi there, everyone"
 
-window = gui.Window(300,100, 'Hello')
-# button = guitk._guitk.Button(text="Hello", command=say_hi)
-button = gui.Button("Hello")
-window.content = button
+window = gui.Window(300,50, 'Hello')
+grid = layout.Grid(1,2)
+button1 = gui.Button("QUIT")
+button2 = gui.Button("Hello")
+button1.hexpand = True
+button2.hexpand = True
+button1.vexpand = True
+button2.vexpand = True
+button1.halign = 'RIGHT'
+button2.halign = 'LEFT'
+button1.foreground = 'Red'
+button1.background = 'LightGray'
+button2.background = 'LightGray'
+grid[0,0] = button1
+grid[0,1] = button2
+window.content = grid
 window.show()
-
